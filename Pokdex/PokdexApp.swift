72129fd13469408ca5a2902d598b550d52514308
Bree Yard <Bwyard@gmail.com>
2021-10-04 19:34:17 -0500

    Initial Commit

---
+++ Pokdex/PokdexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokdexApp.swift
+//  Pokdex
+//
+//  Created by Dawn Moses on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct PokdexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
